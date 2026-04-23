f0c723044b1c261b6ba36f0b8874fef87e8b4936
léo esnault <dcqzdqd@gmail.com>
2026-04-23 14:28:32 +0200

    améliore le lazy loading

--- front/src/view/dashboardPage.vue
+++ front/src/view/dashboardPage.vue
@@ -444,7 +444,7 @@ const handleScroll = async () => {
       );
       
       // AUGMENTATION LINÉAIRE STRICTE
-      THUMBNAILS_BATCH_SIZE.value = currentBatch + 10;
+      THUMBNAILS_BATCH_SIZE.value = currentBatch + 30;
       
 
       // On attend que les images soient traitées avant de libérer le scroll
